5b51b5e2e621318080ddf41009cdffae64921cb3
Samarth <samarthurane3201@gmail.com>
2024-04-13 20:04:50 +0530

    invest and saving updated

--- components/ui/intable.tsx
+++ components/ui/intable.tsx
@@ -1,4 +1,6 @@
-import React from 'react'; 
+'use client'
+import React from 'react';
+import { deleteInvestment } from '@/actions/delete-investment'; 
 
 // Define the type for the dummyEntries data
 interface Entry {
@@ -20,6 +22,13 @@ interface TableProps {
 
 // Table component
 const Table: React.FC<TableProps> = ({ investmentsData }) => {
+
+    const handleDelete = (e:any)=>{
+        e.preventDefault();
+        console.log("LLLLLL")
+        console.log(e.target.id)
+        deleteInvestment({id:e.target.id})
+    }
     return (
         <div> 
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg m-2">
@@ -56,7 +65,7 @@ const Table: React.FC<TableProps> = ({ investmentsData }) => {
                                 <td className="px-6 py-4">{entry.createdAt.toDateString()}</td>
                                 <td className="px-6 py-4">{entry.familyMemberName}</td>
                                 <td className="px-6 py-4 text-right">
-                                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
+                                    <button className="font-medium text-blue-600 dark:text-blue-500 hover:underline" id={entry.id} onClick={handleDelete}>Delete</button>
                                 </td>
                             </tr>
                         ))}
