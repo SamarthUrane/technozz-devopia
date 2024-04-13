39aff598c55022290199d0c8e702e814f06659b7
Prathamesh Thakare <hello@example.com>
2024-04-13 21:04:23 +0530

    home

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,21 +1,77 @@
 import { auth, UserButton } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import {Cabin} from "next/font/google"
 
+import Image from "next/image";
+const font = Cabin({subsets:["latin"]})
 export default function Home() {
-  
-  const {userId} = auth();
-
-  if(userId){
-    redirect("/dash-board");
-  }
-
   return (
-    <div>
-      <div>
-        <h1 className="text-xl font-semibold">
-          Take Control of Your Finances.
-        </h1>
-      </div>
-    </div>
+     
+<div className="flex h-full w-full bg-blue-300 items-center justify-center gap-20 p-20">
+<div className="flex-col w-50 h-25 p-20 shadow-xl bg-amber-200 border-black-200 rounded-xl  dark:border-gray-700 gap-5">
+<h5 className="mb-3 text-base text-center font-semibold text-white md:text-xl dark:text-white">
+Take Control of Your Finances<br/>💰💸<br/>
+          With the best financial management platform<br/>
+</h5>
+<p className="text-sm pb-5 font-normal text-center text-white">Explore our range of services designed to meet your needs !!!</p>
+
+<div className="flex gap-5">
+<div>
+<ul className="my-4 space-y-3">
+<li>
+<a href="#" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+<Image src="/finance.svg" alt="icon" height={30} width={30}/>
+<span className="flex-1 ms-3 whitespace-nowrap">Financial Dashboard</span>
+</a>
+</li>
+<li>
+<a href="#" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+<Image src="/risk.svg" alt="icon" height={30} width={30}/>
+<span className="flex-1 ms-3 whitespace-nowrap">Risk Assessment</span>
+</a>
+</li>
+<li>
+<a href="#" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+<Image src="/family.svg" alt="icon" height={30} width={30}/>
+<span className="flex-1 ms-3 whitespace-nowrap">Family Fanancial Management</span>
+</a>
+</li>
+</ul>
+</div>
+
+<div>
+<ul className="my-4 space-y-3">
+<li>
+<a href="#" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+<Image src="/calculator.svg" alt="icon" height={30} width={30}/>
+<span className="flex-1 ms-3 whitespace-nowrap">Net Worth Calculator</span>
+</a>
+</li>
+<li>
+<a href="#" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+<Image src="/recommendation.svg" alt="icon" height={30} width={30}/>
+<span className="flex-1 ms-3 whitespace-nowrap">Personalized Recommendations</span>
+</a>
+</li>
+<li>
+<a href="#" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+<Image src="/revenue.svg" alt="icon" height={30} width={30}/>
+<span className="flex-1 ms-3 whitespace-nowrap">Risk Adjustment Guide</span>
+</a>
+</li>
+</ul>
+</div>
+</div>
+
+
+</div>
+
+
+<div>
+<Image src="/bitcoin.gif" width={300} height={300} alt="Picture of the author"/>
+</div>
+
+</div>
+   
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,29 +1,33 @@
 import Link from "next/link"
 import { Button } from "./ui/button"
 import { auth, UserButton } from "@clerk/nextjs"
+import {Cabin} from "next/font/google"
+const font = Cabin({subsets:["latin"]})
 
 export const Navbar = async () => {
 
     const {userId} = await auth();
 
     return(
-        <div className="fixed h-16 w-full border-b border-black flex items-center p-2 px-16">
-            <h1>
-                Fintech
+        <div className="fixed h-16 w-full italic border-b border-black flex items-center p-2 px-16 ">
+            <h1 className="text-white text-xl shadow-sm">
+                <b>Wealth Wise</b>
             </h1>
+
             {!userId ? (
                 <div className="ml-auto flex gap-2">
-                    <Button>
-                        <Link href="/sign-in">
-                            Sign In
-                        </Link>
-                    </Button>
-                    <Button>
-                        <Link href="/sign-up">
-                            Sign Up
-                        </Link>
-                    </Button>
-                </div>
+                <Button className="transition ease-in-out delay-10 hover:translate-y-1 hover:scale-110 hover:bg-black duration-30 ...">
+                    <Link href="/sign-in">
+                        Sign In
+                    </Link>
+                </Button>
+                <Button className="transition ease-in-out delay-10 hover:translate-y-1 hover:scale-110 hover:bg-black duration-30 ...">
+                    <Link href="/sign-up">
+                        Sign Up
+                    </Link>
+                </Button>
+            </div>
+
             ): (
                 <div className="ml-auto">
                     <UserButton afterSignOutUrl="/"/>
